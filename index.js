c688d3d723623d34a4d329590b26eb846c1ab783
name <namulishakirah35@gmail.com>
2021-11-02 22:53:47 +0300

    my last commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+console.log("hello uganda ")
+
+document.write("<h1>My name is hamza <h1>");
+
+window.alert("we have a new message");
+
+document.write (" I love  coding "); 
+
+document.write("and i want to be a progammer");
